eeb9b6328c8ce0b2f1ab7f6a12d46470db6990e9
Valemo69 <vmorandi@intramed.net>
2026-05-28 16:35:36 -0300

    Agrego capa Dominio con usuario

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
