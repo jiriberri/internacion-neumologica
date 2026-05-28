0e82a497a3b7292511bf8ffc7792d07124d5057e
Valemo69 <vmorandi@intramed.net>
2026-05-28 17:41:35 -0300

    Agrega capas Dominio y Datos con conexion inicial SQL

--- Datos/AccesoDatos.cs
+++ Datos/AccesoDatos.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
 
-namespace Datos
+namespace Datos// puente de comunicacion con la db
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("Data Source=.\\Initial Catalog=INTERNACION_NEUMOLOGICA_DB;Integrated Security=True");
+        }
     }
 }
