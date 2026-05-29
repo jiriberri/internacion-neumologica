450efd8de285b8f25c9279700b271ebcd1759098
Valemo69 <vmorandi@intramed.net>
2026-05-29 15:09:58 -0300

    Agrega AccesoDatos y arquitectura por capas

--- Datos/AccesoDatos.cs
+++ Datos/AccesoDatos.cs
@@ -10,11 +10,42 @@ namespace Datos// puente de comunicacion con la db
 {
     public class AccesoDatos
     {
-        private SqlConnection conexion;
+        private SqlConnection conexion;//conexion fisica a la db
+        private SqlCommand comando;//consulta a ejecutar
+        private SqlDataReader lector;//lector de db, resultado de la consulta
 
-        public AccesoDatos()
+        public SqlDataReader Lector//
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()//constructor
         {
             conexion = new SqlConnection("Data Source=.\\Initial Catalog=INTERNACION_NEUMOLOGICA_DB;Integrated Security=True");
+            comando = new SqlCommand();
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            conexion.Open();
+
+            lector = comando.ExecuteReader();
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+
+            conexion.Close();
         }
     }
 }
